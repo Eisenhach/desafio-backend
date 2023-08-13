9eb90c8873f52a46c9007366e6bbbcd74f1ecf30
Tomas Eisenhach Pablo <eisenhach.tomas@gmail.com>
2023-08-13 20:54:23 -0300

    final

--- index.js
+++ index.js
@@ -1,51 +1,104 @@
-class productManager{
-    static id = 0;
-    nombre;
-    products;
 
-    constructor(nombre){
-        this.nombre = nombre;
-        this.products = [];
+
+   const fs = require('fs')
+
+class productManager {
+  static id = 0;
+
+  products;
+
+  constructor() {
+    this.products = [];
+    this.path = "./products.json";
+  }
+
+  async getProducts() {
+    const data = JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
+    return data;
+  }
+
+  async getId() {
+    let data = await this.getProducts();
+    return data.length + 1;
+  }
+
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    const newProduct = {
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+
+    try {
+      if (!fs.existsSync(this.path)) {
+        const emptyList = [];
+        emptyList.push({ ...newProduct, id: await this.getId() });
+
+        await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(emptyList, null, "\t")
+        );
+      } else {
+        const data = await this.getProducts();
+        const codigoRepetido = data.some((e) => e.code === newProduct.code);
+        codigoRepetido == true
+          ? console.log("El codigo esta siendo repetido")
+          : data.push({ ...newProduct, id: await this.getId() });
+        await fs.promises.writeFile(this.path, JSON.stringify(data, null));
+      }
+    } catch (err) {
+      console.log(err);
     }
-    
-    construir(title, description, price, thumbrail, code, stock){
-        const producto ={
-            code: productManager.id,
-            title,
-            description,
-            price,
-            thumbrail,
-            stock,
-        }
-    
-        this.products.push(producto)
-        productManager.id++;
-        return producto;
-
-    }
+  }
 
 
-        getProducts(){
-            return this.products
-        }
+  async getProductsById(id){
+    const data = await this.getProducts()
+    let buscarProducto = data.find(e => e.id == id)
+    return buscarProducto === undefined ? console.log("Can't find the ID") : buscarProducto  
+
+  }
+
+  async deleteProduct(id){
+    const data = await this.getProducts()
+    let i = data.findIndex(e=> e.id === id)
+    data.splice(i,1)
+    await fs.promises.writeFile(this.path, JSON.stringify(data))
+  }
 
 
-        getProductById(productCode) {
-            const product = this.products.find(product => product.code === productCode);
-            if (product) {
-                return product;
-            } else {
-                console.log('Product not found');
-                return null;
-            }
-        }
+  async updateProducts (id, product){
+    let data = await this.getProducts()
+    let i = data.findIndex(e=> e.id === id)
+    product.id = id;
+    data.splice(i,1,product)
+    await fs.promises.writeFile(this.path, JSON.stringify(data))
+  }
 
 }
-  
-const manager = new productManager('Manager');
-const productItem = manager.construir('Antorcha de minecraft', 'Muy luminosa', 50,'not image found', 25 )
-const productItem2 = manager.construir('Espada de diamante', 'Encantada', 500,'not image found', 10 )
- console.log(manager.getProducts())     
- 
 
- // :)
+const resultadoAsyncrono = async () =>{
+    const manager = new productManager()
+    console.log(await manager.getProducts())
+    await manager.addProduct('Pruebita', 'Productazo', 2500,'no img','aaa',2);
+    await manager.getProducts()
+    await manager.addProduct('Pruebita', 'Productazo', 2500,'no img','aaa',2)
+    console.log(await manager.getProducts())
+    
+    await manager.getProductsById(2)
+    await manager.addProduct("producto prueba 2 ","Este es un producto prueba 23",150,"Sin imagen","abc1231",25)
+    await productManager.updateProducts(2,{"title": "producto prueba 25 ","description": "Este es un producto prueba 23","price": 150,"thumbnail": "Sin imagen","code": "abc1231","stock": 25,"id": 5})
+    await productManager.deleteProduct(2)
+   
+};
+
+resultadoAsyncrono()
+
+
+
+
+
+
