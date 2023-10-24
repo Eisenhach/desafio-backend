c66a3f70c715eb3fad8a3a3c099c0a4d71e32200
Tomas Eisenhach Pablo <eisenhach.tomas@gmail.com>
2023-10-23 23:10:22 -0300

    add cartId

--- src/dao/models/user.model.js
+++ src/dao/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { cartModel } from "./cart.model.js";
 
 const userCollection = "usuarios";
 
@@ -9,6 +10,11 @@ const userSchema = new mongoose.Schema({
   age: Number,
   password: String,
   role: { type: String, default: "usuario" },
+  cartId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "carts",
+    default: "651cc09a80586b8b948e267d",
+  },
 });
 
 const userModel = mongoose.model(userCollection, userSchema);
